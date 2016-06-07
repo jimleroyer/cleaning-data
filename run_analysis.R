3a343b466d9c4b499a749a4183aae30242616870
Jimmy Royer <jroyer@expedia.com>
2016-06-06 20:50:29 -0400

    Added comment

--- run_analysis.R
+++ run_analysis.R
@@ -32,7 +32,7 @@ activity <- rbind(test_y, train_y)
 x <- rbind(test_x, train_x)
 
 # Associates the activity ID with its matching activity label
-activities$activity <- tolower(activities$activity)
+activities$activity <- tolower(activities$activity) # lower activity labels
 activity <- activity %>% inner_join(activities, by = "activity_id")
 
 # Merge results of columns altogether
